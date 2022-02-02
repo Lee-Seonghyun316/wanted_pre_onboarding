fd0e4f8599edb3aa92b67f434606537fc457f54f
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-02-02 23:07:55 +0900

    Add: 개발 환경 세팅

---
+++ custom-component/src/component/Toggle.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Toggle = (props) => <Wrap>hello</Wrap>;
+
+export default Toggle;
+
+const Wrap = styled.div`
+  background: pink;
+`;
