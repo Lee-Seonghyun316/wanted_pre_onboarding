e1931e5ba4bb88a80cbc8ff30da2ae7020ad2861
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-02-04 06:34:07 +0900

    Docs: jsx -> js
    - 필요에 따른 파일명 및 확장자 변경

--- custom-component/src/component/Modal.js
+++ custom-component/src/component/Modal.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Container from "./Container";
-import BodyBlackout from "./bodyBlackout";
+import BodyBlackout from "./BodyBlackout";
 
 const Modal = ({ modalVisible, changeModalVisible }) => (
   <Container title="Modal">
