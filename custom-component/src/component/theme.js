90d94b065cc80fb38084baa4ba22bfa1edfd8418
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-02-05 21:43:32 +0900

    Style: theme.js 에서 css 필요한 값 관리

---
+++ custom-component/src/component/theme.js
@@ -0,0 +1,31 @@
+const colors = {
+  black: "#161212",
+  white: "#ffffff",
+  gray_1: "#a1a1a1",
+  gray_2: "#adadad",
+  gray_3: "#c5c5c5",
+  gray_4: "#e0e0e0",
+  gray_5: "#d1d1d1",
+  purple: "#4900ce",
+};
+
+const fontWeight = {
+  bold_500: 500,
+  bold_600: 600,
+  bold_700: 700,
+  bold_800: 800,
+  bold_900: 900,
+};
+
+const fontSize = {
+  normal: "0.9rem",
+  small: "0.8rem",
+};
+
+const theme = {
+  colors,
+  fontWeight,
+  fontSize,
+};
+
+export default theme;
