769a8402d72f551c3ef57ab922e534350bd95823
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-02-09 01:22:12 +0900

    Refactor: Toggle 중복 css 변수 분리

--- custom-component/src/component/Toggle.js
+++ custom-component/src/component/Toggle.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import Container from "./Container";
 
 const [ON, OFF] = ["ON", "OFF"];
@@ -20,6 +20,24 @@ const Toggle = () => {
 
 export default Toggle;
 
+const backgroundStyle = css`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  height: 100%;
+`;
+
+const circleStyle = css`
+  position: absolute;
+  top: calc((2rem - 1.6em) / 2);
+  width: 1.6em;
+  height: 1.6em;
+  z-index: 2;
+  border-radius: 50%;
+  background: ${({ theme }) => theme.colors.white};
+`;
+
 const ToggleButton = styled.input`
   width: 5rem;
   height: 2rem;
@@ -29,53 +47,38 @@ const ToggleButton = styled.input`
   position: relative;
   // 체크안함
   &::before {
+    //토글 배경
+    ${backgroundStyle};
     content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: 1;
     width: 10%;
-    height: 100%;
     border-radius: 2em 0 0 2em;
     background: none;
     transition: width 0.4s ease-in-out, border-radius 0.2s step-end,
       background 0.37s step-end;
   }
   &::after {
-    z-index: 2;
-    position: absolute;
+    //토글 동그라미
+    ${circleStyle};
     content: "";
-    width: 1.6em;
-    height: 1.6em;
-    top: calc((2rem - 1.6em) / 2);
     left: calc((2rem - 1.6em) / 2);
-    border-radius: 50%;
-    background: ${({ theme }) => theme.colors.white};
     transition: all 0.4s ease-in-out;
   }
   // 체크함
   &:checked {
     &::before {
+      //토글 배경
+      ${backgroundStyle};
       content: "";
-      z-index: 1;
       width: 100%;
-      height: 100%;
       border-radius: 2em;
       background: ${({ theme }) => theme.colors.purple};
-      position: absolute;
       transition: width 0.4s ease-in-out, border-radius 0.3s step-end;
-      top: 0;
     }
-    ::after {
-      position: absolute;
+    &::after {
+      //토글 동그라미
+      ${circleStyle};
       content: "";
-      z-index: 2;
-      top: calc((2rem - 1.6em) / 2);
       left: calc(5rem - 1.9em);
-      width: 1.6em;
-      height: 1.6em;
-      border-radius: 50%;
-      background: ${({ theme }) => theme.colors.white};
     }
   }
 `;
